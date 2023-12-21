1e7cac4cf4a8226e92c66a97b4e47eca2f4628a8
Pixus20 <152525734+Pixus20@users.noreply.github.com>
2023-12-21 16:45:45 +0200

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.menu_burger').addEventListener('click',function(){
-    this.classList.toggle('active');
-    document.querySelector('.main_menu_nav_ul').classList.toggle('open');
-    })
